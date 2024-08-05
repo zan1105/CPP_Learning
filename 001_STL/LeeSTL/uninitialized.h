e62ef121d2a10fc8a93eb1c00c27c18e460f25ff
李赞赞 <leezanzan@outlook.com>
2024-08-05 14:04:30 +0800

    新增uninitialized_copy方法

--- 001_STL/LeeSTL/uninitialized.h
+++ 001_STL/LeeSTL/uninitialized.h
@@ -1,33 +1,68 @@
 /** @file uninitialized.h
- * 	这个文件实现在未初始化空间构造对象
+ * 	这个文件实现在未初始化空间构造对象的相关方法
  */
 
 #ifndef _LEESTL_UNINITIALIZED_H_
 #define _LEESTL_UNINITIALIZED_H_ 1
 
+#include "algobase.h"
+#include "construct.h"
 #include "utils.h"
 #include "iterator.h"
 
 namespace leestl {
 
-	template <typename _InputIterator, typename _ForwardIterator>
-	inline _ForwardIterator uninitialized_copy(
-	    _InputIterator first, _InputIterator last, _ForwardIterator result) {}
+	// 未做初始化的的复制构造方法(可基于移动的复制)
+	template <typename _II, typename _FI>
+	_FI _unchecked_uninit_copy(_II first, _II last, _FI result, leestl::true_type) {
+		return leestl::copy(first, last, result);
+	}
+
+	// 未做初始化的的复制构造方法
+	template <typename _II, typename _FI>
+	_FI _unchecked_uninit_copy(_II first, _II last, _FI result, leestl::false_type) {
+		auto curr = result;
+		try {
+			for (; first != last; ++first, (void)++curr)
+				leestl::construct(leestl::address_of(*curr), *first);
+			return curr;
+		} catch (...) {
+			leestl::destory(result, curr);
+			throw;
+		}
+	}
 
 	/**
 	 * @brief 批量复制构造方法，把 [first, last) 上的内容复制到以 result 为起始处的空间
 	 *
-	 * @tparam _InputIterator 输入迭代器类型
-	 * @tparam _ForwardIterator 前向迭代器类型
+	 * @tparam _II 原始空间的迭代器类型
+	 * @tparam _FI 目标空间的迭代器类型
 	 * @param first 原始空间的起始位置
 	 * @param last	原始空间的终止位置
 	 * @param result 目标空间的起始位置
-	 * @return _ForwardIterator 目标空间尾部 result + (last - first)
+	 * @return _FI 目标空间尾部 result + (last - first)
 	 */
-	template <typename _InputIterator, typename _ForwardIterator>
-	inline _ForwardIterator uninitialized_move(
-	    _InputIterator first, _InputIterator last, _ForwardIterator result) {}
+	template <typename _II, typename _FI>
+	inline _FI uninitialized_copy(_II first, _II last, _FI result) {
+		return leestl::_unchecked_uninit_copy(
+		    first, last, result,
+		    std::is_trivially_copy_assignable<typename iterator_traits<_FI>::value_type>{});
+	}
 
+	/**
+	 * @brief 移动（复制）构造方法，把 [first, last) 上的内容移动复制到以 result 为起始处的空间
+	 *
+	 * @tparam _II 原始空间的迭代器类型
+	 * @tparam _FI 目标空间的迭代器类型
+	 * @param first 原始空间的起始位置
+	 * @param last	原始空间的终止位置
+	 * @param result 目标空间的起始位置
+	 * @return _FI 目标空间尾部 result + (last - first)
+	 */
+	template <typename _II, typename _FI>
+	inline _FI uninitialized_move(_II first, _II last, _FI result) {}
+
+	// 一个对象的移动构造方法
 	template <typename Tp, typename Up>
 	inline void _relocate_object(Tp *dest, Up *orig) {
 		leestl::construct(dest, leestl::move(*orig));
@@ -35,25 +70,24 @@ namespace leestl {
 	}
 
 	/**
-	 * @brief 批量移动构造方法，把 [first, last) 上的内容移动到以 result 为起始处的空间
+	 * @brief 移动构造方法，把 [first, last) 上的内容移动到以 result 为起始处的空间
 	 *
-	 * @tparam _InputIterator 输入迭代器类型
-	 * @tparam _ForwardIterator 前向迭代器类型
+	 * @tparam _II 原始空间的迭代器类型
+	 * @tparam _FI 目标空间的迭代器类型
 	 * @param first 原始空间的起始位置
 	 * @param last	原始空间的终止位置
 	 * @param result 目标空间的起始位置
-	 * @return _ForwardIterator 目标空间尾部 result + (last - first)
+	 * @return _FI 目标空间尾部 result + (last - first)
 	 */
-	template <typename _InputIterator, typename _ForwardIterator>
-	inline _ForwardIterator relocate(
-	    _InputIterator first, _InputIterator last, _ForwardIterator result) {
-		typedef typename iterator_traits<_InputIterator>::value_type type1;
-		typedef typename iterator_traits<_InputIterator>::value_type type1;
+	template <typename _II, typename _FI>
+	inline _FI relocate(_II first, _II last, _FI result) {
+		typedef typename iterator_traits<_II>::value_type type1;
+		typedef typename iterator_traits<_FI>::value_type type1;
 		static_assert(
 		    std::is_same<type1, type1>::value,
 		    "relocation is only possible for values of the same type");
 
-		_ForwardIterator curr = result;
+		_FI curr = result;
 		for (; first != last; ++first, (void)++curr;)
 			leestl::_relocate_object(leestl::address_of(*curr), leestl::address_of(*first));
 
