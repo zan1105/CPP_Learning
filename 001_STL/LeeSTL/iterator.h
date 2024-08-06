b0d65161273735e59b3af3b42dad7a7a2408a9a8
李赞赞 <leezanzan@outlook.com>
2024-08-06 16:24:12 +0800

    反向迭代器实现及相关运算符重载

--- 001_STL/LeeSTL/iterator.h
+++ 001_STL/LeeSTL/iterator.h
@@ -21,7 +21,126 @@ namespace leestl {
 	                             typename iterator_traits<_Iterator>::reference> {
 		template <typename _Iter>
 		friend class reverse_iterator;
+
+	protected:
+		_Iterator current;    // 当前迭代器
+
+	public:
+		typedef _Iterator iterator_type;
+
+		// 构造、复制、移动、析构函数
+		reverse_iterator() : current() {}
+		explicit reverse_iterator(iterator_type __x) : current(__x) {}
+		template <typename _Iter>
+		reverse_iterator(const reverse_iterator<_Iter>& __x) : current(__x.base()) {}
+
+		// 获取当前迭代器
+		iterator_type base() const { return current; }
+
+		// 重载运算符
+		reference operator*() const {
+			_Iterator __tmp = current;
+			return *--__tmp;
+		}
+
+		pointer operator->() const { return &(operator*()); }
+
+		reverse_iterator& operator++() {
+			--current;
+			return *this;
+		}
+
+		reverse_iterator operator++(int) {
+			reverse_iterator __tmp = *this;
+			--current;
+			return __tmp;
+		}
+
+		reverse_iterator& operator--() {
+			++current;
+			return *this;
+		}
+
+		reverse_iterator operator--(int) {
+			reverse_iterator __tmp = *this;
+			++current;
+			return __tmp;
+		}
+
+		reverse_iterator operator+(difference_type __n) const {
+			return reverse_iterator(current - __n);
+		}
+
+		reverse_iterator& operator+=(difference_type __n) {
+			current -= __n;
+			return *this;
+		}
+
+		reverse_iterator operator-(difference_type __n) const {
+			return reverse_iterator(current + __n);
+		}
+
+		reverse_iterator& operator-=(difference_type __n) {
+			current += __n;
+			return *this;
+		}
+
+		reference operator[](difference_type __n) const { return *(*this + __n); }
 	};
+
+	template <typename _Iterator>
+	inline bool operator==(
+	    const reverse_iterator<_Iterator>& __x, const reverse_iterator<_Iterator>& __y) {
+		return __x.base() == __y.base();
+	}
+
+	template <typename _Iterator>
+	inline bool operator<(
+	    const reverse_iterator<_Iterator>& __x, const reverse_iterator<_Iterator>& __y) {
+		return __y.base() < __x.base();
+	}
+
+	template <typename _Iterator>
+	inline bool operator!=(
+	    const reverse_iterator<_Iterator>& __x, const reverse_iterator<_Iterator>& __y) {
+		return !(__x == __y);
+	}
+
+	template <typename _Iterator>
+	inline bool operator>(
+	    const reverse_iterator<_Iterator>& __x, const reverse_iterator<_Iterator>& __y) {
+		return __y < __x;
+	}
+
+	template <typename _Iterator>
+	inline bool operator<=(
+	    const reverse_iterator<_Iterator>& __x, const reverse_iterator<_Iterator>& __y) {
+		return !(__y < __x);
+	}
+
+	template <typename _Iterator>
+	inline bool operator>=(
+	    const reverse_iterator<_Iterator>& __x, const reverse_iterator<_Iterator>& __y) {
+		return !(__x < __y);
+	}
+
+	template <typename _Iterator>
+	inline typename reverse_iterator<_Iterator>::difference_type operator-(
+	    const reverse_iterator<_Iterator>& __x, const reverse_iterator<_Iterator>& __y) {
+		return __y.base() - __x.base();
+	}
+
+	template <typename _Iterator>
+	inline reverse_iterator<_Iterator> operator+(
+	    typename reverse_iterator<_Iterator>::difference_type __n,
+	    const reverse_iterator<_Iterator>&                    __x) {
+		return reverse_iterator<_Iterator>(__x.base() - __n);
+	}
+
+	template <typename _Iterator>
+	inline reverse_iterator<_Iterator> make_reverse_iterator(_Iterator __i) {
+		return reverse_iterator<_Iterator>(__i);
+	}
 
 }    // namespace leestl
 
