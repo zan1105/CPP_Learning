9d889576f112d1ebca5ec2741eade66ee735e3bc
李赞赞 <leezanzan@outlook.com>
2024-08-15 15:36:30 +0800

    赋值运算符重载测试

--- 001_STL/test/vector.cpp
+++ 001_STL/test/vector.cpp
@@ -39,20 +39,29 @@ int main() {
 	cout << "[---------------------- vector test start ---------------------->\n";
 	cout << "[------------------- API test start------------------->\n";
 	leestl::vector<int> v1, v2(10), v3(10, 5);
-	leestl::vector<int> v4 = {1, 2, 3, 4, 5};
+	leestl::vector<int> v4 = {41, 42, 43, 44, 45};
 	leestl::vector<int> v5(v4.begin(), v4.end()), v6(v3);
-
+	leestl::vector<int> v7, v8(leestl::move(v4)), v9, v10;
+	v7 = v4;
+	v9 = {91, 92, 93, 94, 95};
 	cout << "v1: " << v1;
 	cout << "v2: " << v2;
 	cout << "v3: " << v3;
 	cout << "v4: " << v4;
 	cout << "v5: " << v5;
 	cout << "v6: " << v6;
-	cout << "v4.size(): " << v4.size() << endl;
-	cout << "v4.max_size(): " << v4.max_size() << endl;
-	cout << "v4.capacity(): " << v4.capacity() << endl;
-	cout << "v4.empty(): " << v4.empty() << endl;
-	cout << "v1.empty(): " << v1.empty() << endl;
+	cout << "v7: " << v7;
+	cout << "v8: " << v8;
+	cout << "v9: " << v9;
+
+	v10 = leestl::move(v9);    // 移动赋值实现了对象移动
+	cout << "v9: " << v9;
+	cout << "v10: " << v10;
+
+	leestl::vector<int> v11(leestl::move(v10));    // 移动构造使用默认构造函数，并未进行移动
+	cout << "v10: " << v10;
+	cout << "v11: " << v11;
+
 	cout << "v4-reverse_iterator: ";
 	rcout(v4);
 
